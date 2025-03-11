4168508a4298ea4903d391db22bdf2cbcf71ff01
ray_rid <rid843@mail.ru>
2025-03-11 19:38:06 +0700

    Добавлено 2 вида Шипов

---
+++ Assets/formless/Scripts/Room/Traps/Spike.cs
@@ -0,0 +1,45 @@
+using Formless.Enemy;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Formless.Room.Traps
+{
+    public class Spike : MonoBehaviour
+    {
+        private PolygonCollider2D _polygonCollider;
+        private float _trapDamage = 1f;
+
+        private void Awake()
+        {
+            _polygonCollider = GetComponent<PolygonCollider2D>();
+        }
+
+        private void Start()
+        {
+            _polygonCollider.enabled = false;
+        }
+
+        private void EnableCollider()
+        {
+            _polygonCollider.enabled = true;
+        }
+
+        private void DisableCollider()
+        {
+            _polygonCollider.enabled = false;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.transform.TryGetComponent(out Enemy.Enemy enemy))
+            {
+                enemy.TakeDamage(_trapDamage);
+            }
+            if (collision.transform.TryGetComponent(out Player.Player player))
+            {
+                player.TakeDamage(_trapDamage);
+            }
+        }
+    }
+
+}
